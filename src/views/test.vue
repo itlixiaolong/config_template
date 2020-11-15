fd1489ab9cceae85dab078dfac605e2850921123
lixiaolong02 <lixiaolong02@longfor.com>
2020-11-15 19:50:53 +0800

    workflow(gloabl): 增加commit的校验

---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    <!--html code-->
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Test',
+  components: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+.test {
+  /* less */
+}
+
+</style>
